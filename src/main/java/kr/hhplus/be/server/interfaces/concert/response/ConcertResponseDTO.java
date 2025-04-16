778dc5dba936fcd772b77a9d0f66119b99b00de0
LK920 <hopeone1214@gmail.com>
2025-04-17 02:11:19 +0900

    refact: 콘서트 인터페이스 수정

--- src/main/java/kr/hhplus/be/server/interfaces/concert/response/ConcertResponseDTO.java
+++ src/main/java/kr/hhplus/be/server/interfaces/concert/response/ConcertResponseDTO.java
@@ -2,6 +2,7 @@ package kr.hhplus.be.server.interfaces.concert.response;
 
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import kr.hhplus.be.server.domain.concert.ConcertInfo;
 
 public record ConcertResponseDTO(
         @Schema(description = "콘서트 id", example = "1")
@@ -11,5 +12,11 @@ public record ConcertResponseDTO(
         @Schema(description = "콘서트 총 좌석 수 ", example = "50")
         long concertTotalSeats
 ){
-
+        public static ConcertResponseDTO from(ConcertInfo concertInfo){
+                return new ConcertResponseDTO(
+                        concertInfo.concertId(),
+                        concertInfo.concertName(),
+                        concertInfo.concertTotalSeat()
+                );
+        }
 }
