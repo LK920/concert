fe0210e5576eeaee26430671c7dd1ebeb5ba64d6
LK920 <hopeone1214@gmail.com>
2025-04-17 01:56:54 +0900

    feat : 콘서트 레포지토러 구현체 추가 test : 콘서트 통합테스트 추가

--- src/main/java/kr/hhplus/be/server/domain/concert/concertVO/ConcertName.java
+++ src/main/java/kr/hhplus/be/server/domain/concert/concertVO/ConcertName.java
@@ -1,10 +1,14 @@
 package kr.hhplus.be.server.domain.concert.concertVO;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Value;
-
-@Value
+import jakarta.persistence.Embeddable;
+import lombok.*;
+/*
+* Embaddable => entity의 vo를 칼럼처럼 사용하게 해준다.
+* jpa 엔터티로 만들고
+* */
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class ConcertName {
     String concertName;
 
