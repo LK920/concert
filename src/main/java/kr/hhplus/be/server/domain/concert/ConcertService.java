760486b7bce67dd623d1838060be2c1ba1435498
LK920 <hopeone1214@gmail.com>
2025-05-09 00:48:56 +0900

    feat: 캐싱적용( 포인트 조회, 콘서트 조회, 콘서트 날짜 조회)

--- src/main/java/kr/hhplus/be/server/domain/concert/ConcertService.java
+++ src/main/java/kr/hhplus/be/server/domain/concert/ConcertService.java
@@ -1,6 +1,8 @@
 package kr.hhplus.be.server.domain.concert;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -12,13 +14,15 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ConcertService {
 
     private final ConcertRepository concertRepository;
 
+    @Cacheable(value = "concert", key = "'concertList'")
+    @Transactional(readOnly = true)
     public List<ConcertInfo> getConcerts(){
         List<Concert> concertList = concertRepository.findAllConcerts();
-
         return concertList.stream().map(concert -> ConcertInfo.from(concert)).toList();
     }
 }
--- src/main/java/kr/hhplus/be/server/domain/point/PointService.java
+++ src/main/java/kr/hhplus/be/server/domain/point/PointService.java
@@ -4,6 +4,8 @@ import jakarta.persistence.EntityNotFoundException;
 import kr.hhplus.be.server.support.enums.LockType;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.boot.model.naming.IllegalIdentifierException;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,11 +15,14 @@ public class PointService {
 
     private final PointRepository pointRepository;
 
+    @Cacheable(value = "userPoint", key = "#userId")
     @Transactional(readOnly = true)
     public PointInfo getUserPoint(long userId){
         Point point = pointRepository.findByUserId(userId).orElseThrow(()->new EntityNotFoundException("해당 유저가 없습니다."));
         return PointInfo.from(point);
     }
+
+    @CacheEvict(value = "userPoint", key = "#userId")
     @Transactional
     public PointInfo chargeUserPoint(long userId, long amount){
         // 포인트 데이터
@@ -28,6 +33,7 @@ public class PointService {
         return PointInfo.from(saved);
     }
 
+    @CacheEvict(value = "userPoint", key = "#userId")
     @Transactional
     public void useUserPoint(long userId, long amount){
         // 포인트 데이터
