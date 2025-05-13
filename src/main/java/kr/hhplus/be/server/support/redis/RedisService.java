d05b97a0a46c4d500bbc78002869088e3532dd66
LK920 <hopeone1214@gmail.com>
2025-05-13 16:51:35 +0900

    test: sorted set command 테스트

--- src/main/java/kr/hhplus/be/server/support/redis/RedisService.java
+++ src/main/java/kr/hhplus/be/server/support/redis/RedisService.java
@@ -1,13 +1,27 @@
 package kr.hhplus.be.server.support.redis;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RScoredSortedSet;
+import org.redisson.api.RSortedSet;
+import org.redisson.api.RedissonClient;
+import org.redisson.client.codec.StringCodec;
+import org.redisson.client.protocol.ScoredEntry;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class RedisService {
-
+    private final RedissonClient redissonClient;
     private final RedisTemplate<String, Object> redisTemplate;
 
     public void save(String key, String value){
@@ -27,4 +41,24 @@ public class RedisService {
         redisTemplate.getConnectionFactory().getConnection().serverCommands().flushAll();
     }
 
+    public void zAdd(String key, long score, String member){
+        /*
+        * redisson RSortedSet ->redis 자료형 list, set
+        * redisson RScoreSortedSet -> redis 자료현 zSet
+        * codec 설정을 해줘야지 직렬화 없이 string으로 저장
+        * */
+        RScoredSortedSet<String> zSet = redissonClient.getScoredSortedSet(key, StringCodec.INSTANCE);
+
+        zSet.add(score, member); // member의 score를 update(없으면 생성)
+    }
+
+    public double zIncrBy(String key, double score, String member ){
+        RScoredSortedSet<String> zSet = redissonClient.getScoredSortedSet(key, StringCodec.INSTANCE);
+        return zSet.addScore(member,score); //member의 score를 추가
+    }
+
+     public Collection<ScoredEntry<String>> zRangeByScore(String key){
+        RScoredSortedSet<String> zSets = redissonClient.getScoredSortedSet(key, StringCodec.INSTANCE);
+        return zSets.entryRange(0,-1);
+    }
 }
--- src/test/java/kr/hhplus/be/server/RedisServiceTest.java
+++ src/test/java/kr/hhplus/be/server/RedisServiceTest.java
@@ -1,20 +1,36 @@
 package kr.hhplus.be.server;
 
 import kr.hhplus.be.server.support.redis.RedisService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.redisson.Redisson;
+import org.redisson.api.RScoredSortedSet;
+import org.redisson.api.RedissonClient;
+import org.redisson.client.codec.StringCodec;
+import org.redisson.client.protocol.ScoredEntry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Random;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Slf4j
 public class RedisServiceTest {
 
     @Autowired
     private RedisService redisService;
+
+    @Autowired
+    private RedissonClient redissonClient;
 
     @Test
     void redisRegisterStrings(){
@@ -38,6 +54,36 @@ public class RedisServiceTest {
         Object result = redisService.get(key);
 
         assertThat(result).isNull();
+    }
+
+    @Test
+    void redissonZAdd(){
+        LocalDate now = LocalDate.now();
+        DateTimeFormatter ofPattern = DateTimeFormatter.ofPattern("yyyyMMdd");
+        String key = "concert:" + now.format(ofPattern);
+        int count = 10;
+        Random random = new Random();
+        for(int i = 1; i < count; i ++){
+            String member = "item" + i;
+            int number = random.nextInt(10) + 1;
+            redisService.zAdd(key, number, member);
+        }
+
+        Collection<ScoredEntry<String>> zSets = redisService.zRangeByScore(key);
+        zSets.stream().forEach(zset -> log.info("member : {}, score : {}", zset.getValue(), zset.getScore()));
+    }
+
+    @Test
+    void zIncrBy(){
+        String key = "concert:20250513";
+        String member = "item4";
+        double score = 12;
+
+        double updatedScore = redisService.zIncrBy(key,score, member);
+
+        RScoredSortedSet<String> zSets = redissonClient.getScoredSortedSet(key, StringCodec.INSTANCE);
+
+        assertThat(zSets.getScore(member)).isEqualTo(updatedScore);
     }
 
 }
