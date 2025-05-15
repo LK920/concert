c6dc79d7e7758b0d59801af57c2dafbd3959c5c3
LK920 <hopeone1214@gmail.com>
2025-05-16 01:21:54 +0900

    refactor: 대기열 서비스의 변경으로  컨트롤러 및 스케줄러 수정

--- src/main/java/kr/hhplus/be/server/interfaces/scheduler/Scheduler.java
+++ src/main/java/kr/hhplus/be/server/interfaces/scheduler/Scheduler.java
@@ -1,6 +1,7 @@
 package kr.hhplus.be.server.interfaces.scheduler;
 
 import kr.hhplus.be.server.domain.queue.WaitingQueueService;
+import kr.hhplus.be.server.domain.queue.redis.RedisWaitingQueueService;
 import kr.hhplus.be.server.domain.ranking.RankingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -13,12 +14,12 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 public class Scheduler {
 
-    private final WaitingQueueService waitingQueueService;
+    private final RedisWaitingQueueService waitingQueueService;
     private final RankingService rankingService;
 
     @Scheduled(fixedRateString = "${scheduler.fix-rate-ms}")
     public void runScheduler(){
-        waitingQueueService.refreshWaitingQueueStatus(LocalDateTime.now());
+        waitingQueueService.refreshWaitingQueueStatus();
     }
 
     @Scheduled(cron = "0 0 0 * * *") // 매일 00시
--- src/main/java/kr/hhplus/be/server/interfaces/waitingQueue/response/ResponseQueue.java
+++ src/main/java/kr/hhplus/be/server/interfaces/waitingQueue/response/ResponseQueue.java
@@ -1,6 +1,7 @@
 package kr.hhplus.be.server.interfaces.waitingQueue.response;
 
 import kr.hhplus.be.server.domain.queue.WaitingQueueDetail;
+import kr.hhplus.be.server.domain.queue.redis.RedisQueueStatusResponse;
 
 public record ResponseQueue(
     String token,
@@ -11,5 +12,9 @@ public record ResponseQueue(
 
     public static ResponseQueue from(WaitingQueueDetail waitingQueueDetail){
         return  new ResponseQueue(waitingQueueDetail.token(), waitingQueueDetail.remainedMillis(), waitingQueueDetail.status().toString(), waitingQueueDetail.waitingNumber());
+    }
+
+    public static ResponseQueue fromRedis(RedisQueueStatusResponse statusResponse){
+        return new ResponseQueue(statusResponse.token(), statusResponse.remainedMillis(), statusResponse.status().toString(), statusResponse.waitingNumber());
     }
 }
