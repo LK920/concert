a4723e9045c5b121cca482466d34dd822df81ff1
LK920 <hopeone1214@gmail.com>
2025-05-15 14:23:25 +0900

    feat: 랭킹 스케줄러 추가

--- src/main/java/kr/hhplus/be/server/interfaces/scheduler/Scheduler.java
+++ src/main/java/kr/hhplus/be/server/interfaces/scheduler/Scheduler.java
@@ -1,6 +1,7 @@
 package kr.hhplus.be.server.interfaces.scheduler;
 
 import kr.hhplus.be.server.domain.queue.WaitingQueueService;
+import kr.hhplus.be.server.domain.ranking.RankingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -13,9 +14,18 @@ import java.time.LocalDateTime;
 public class Scheduler {
 
     private final WaitingQueueService waitingQueueService;
+    private final RankingService rankingService;
 
     @Scheduled(fixedRateString = "${scheduler.fix-rate-ms}")
     public void runScheduler(){
         waitingQueueService.refreshWaitingQueueStatus(LocalDateTime.now());
     }
+
+    @Scheduled(cron = "0 0 0 * * *") // 매일 00시
+    public void rankingScheduler() {
+        rankingService.setAggregateRanking(1, "concert:ranking:daily");
+        rankingService.setAggregateRanking(7, "concert:ranking:weekly");
+        rankingService.setAggregateRanking(30, "concert:ranking:monthly");
+    }
+
 }
