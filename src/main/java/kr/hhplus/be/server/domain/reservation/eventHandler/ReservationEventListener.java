fe95782fc4ca3f46a5d08ba5df43682b549b33e7
LK920 <hopeone1214@gmail.com>
2025-05-22 13:39:08 +0900

    feat: 예약 성공/실패 이벤트 추가

---
+++ src/main/java/kr/hhplus/be/server/domain/reservation/eventHandler/ReservationEventListener.java
@@ -0,0 +1,25 @@
+package kr.hhplus.be.server.domain.reservation.eventHandler;
+
+import kr.hhplus.be.server.domain.events.PaymentCreatedEvent;
+import kr.hhplus.be.server.domain.reservation.ReservationInfo;
+import kr.hhplus.be.server.domain.reservation.ReservationService;
+import kr.hhplus.be.server.infra.external.DataPlatformClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Component
+@RequiredArgsConstructor
+public class ReservationEventListener {
+    private final ReservationService reservationService;
+    private final DataPlatformClient dataPlatformClient;
+
+    @Async
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void handlePaymentCreated(PaymentCreatedEvent event){
+        ReservationInfo updated = reservationService.updatePaymentInfo(event.reservationId(), event.paymentId());
+        dataPlatformClient.sendReservation(updated);
+    }
+}
